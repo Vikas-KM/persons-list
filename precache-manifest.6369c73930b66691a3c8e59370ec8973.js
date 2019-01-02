1659e57344181b39741a25ca9a7652d2345a275e
Vikas KM <vikki.km@gmail.com>
2019-01-02 10:40:45 +0530

    Updates

---
+++ precache-manifest.6369c73930b66691a3c8e59370ec8973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "82efe285c3d127f4df59",
+    "url": "/persons-list/static/css/main.265d2a1b.chunk.css"
+  },
+  {
+    "revision": "82efe285c3d127f4df59",
+    "url": "/persons-list/static/js/main.82efe285.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/persons-list/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "46a0e193a9abcd2e2c86",
+    "url": "/persons-list/static/js/runtime~main.46a0e193.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/persons-list/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2197cfe09c51f8d214101cd378f1f241",
+    "url": "/persons-list/index.html"
+  }
+];
